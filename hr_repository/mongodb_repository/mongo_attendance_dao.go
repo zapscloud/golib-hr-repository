c991a3c0514b0088e0b793e58facdc8209704cd9
anbu <anburajan.inforis@gmail.com>
2023-12-29 16:38:45 +0530

    changes

--- hr_repository/mongodb_repository/mongo_attendance_dao.go
+++ hr_repository/mongodb_repository/mongo_attendance_dao.go
@@ -455,28 +455,7 @@ func (p *AttendanceMongoDBDao) DeleteMany() (int64, error) {
 
 func (p *AttendanceMongoDBDao) appendListLookups(stages []bson.M) []bson.M {
 
-	// // Lookup Stage for Token PlatformAppUsers ========================================
-	// lookupStage := bson.M{
-	// 	hr_common.MONGODB_LOOKUP: bson.M{
-	// 		hr_common.MONGODB_STR_FROM:         platform_common.DbPlatformAppUsers,
-	// 		hr_common.MONGODB_STR_LOCALFIELD:   hr_common.FLD_STAFF_ID,
-	// 		hr_common.MONGODB_STR_FOREIGNFIELD: platform_common.FLD_APP_USER_ID,
-	// 		hr_common.MONGODB_STR_AS:           hr_common.FLD_STAFF_INFO,
-	// 		hr_common.MONGODB_STR_PIPELINE: []bson.M{
-	// 			// Remove following fields from result-set
-	// 			{hr_common.MONGODB_PROJECT: bson.M{
-	// 				db_common.FLD_DEFAULT_ID:              0,
-	// 				db_common.FLD_IS_DELETED:              0,
-	// 				db_common.FLD_CREATED_AT:              0,
-	// 				db_common.FLD_UPDATED_AT:              0,
-	// 				hr_common.FLD_BUSINESS_ID:             0,
-	// 				platform_common.FLD_APP_USER_PASSWORD: 0}},
-	// 		},
-	// 	},
-	// }
-	// // Add it to Aggregate Stage Shift
-	// stages = append(stages, lookupStage)
-
+	
 	// Lookup Stage for Clients ========================================
 	lookupStage := bson.M{
 		hr_common.MONGODB_LOOKUP: bson.M{
