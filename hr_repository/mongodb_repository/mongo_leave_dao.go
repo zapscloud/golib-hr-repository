fa27f14672d2058cdf82ee9effe9beb9b9c960d0
anbu <anburajan.inforios@gmail.com>
2024-08-08 12:16:13 +0530

    update

--- hr_repository/mongodb_repository/mongo_leave_dao.go
+++ hr_repository/mongodb_repository/mongo_leave_dao.go
@@ -441,33 +441,10 @@ func (p *LeaveMongoDBDao) DeleteMany() (int64, error) {
 
 func (p *LeaveMongoDBDao) appendListLookups(stages []bson.M) []bson.M {
 
-	// Lookup Stage for staff info ========================================
-	lookupStage := bson.M{
-		hr_common.MONGODB_LOOKUP: bson.M{
-			hr_common.MONGODB_STR_FROM:         hr_common.DbHrStaffs,
-			hr_common.MONGODB_STR_LOCALFIELD:   hr_common.FLD_STAFF_ID,
-			hr_common.MONGODB_STR_FOREIGNFIELD: hr_common.FLD_STAFF_ID,
-			hr_common.MONGODB_STR_AS:           hr_common.FLD_STAFF_INFO,
-			hr_common.MONGODB_STR_PIPELINE: []bson.M{
-				// Match documents where FLD_IS_DELETED is false
-				{db_common.MONGODB_MATCH: bson.M{
-					db_common.FLD_IS_DELETED: false,
-				}},
-				// Remove following fields from result-set
-				{hr_common.MONGODB_PROJECT: bson.M{
-					db_common.FLD_DEFAULT_ID:  0,
-					db_common.FLD_IS_DELETED:  0,
-					db_common.FLD_CREATED_AT:  0,
-					hr_common.FLD_BUSINESS_ID: 0,
-					db_common.FLD_UPDATED_AT:  0}},
-			},
-		},
-	}
-	// Add it to Aggregate Stage
-	stages = append(stages, lookupStage)
+	
 
-	// Lookup Stage for User ==========================================
-	lookupStage = bson.M{
+	// Lookup Stage for leavetype info ==========================================
+	lookupStage := bson.M{
 		hr_common.MONGODB_LOOKUP: bson.M{
 			hr_common.MONGODB_STR_FROM:         hr_common.DbHrLeaveTypes,
 			hr_common.MONGODB_STR_LOCALFIELD:   hr_common.FLD_LEAVETYPE_ID,
